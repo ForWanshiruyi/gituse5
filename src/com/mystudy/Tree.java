7ea45e032afb5efa9c4ae05448df15f681445b25
wanshiruyi <wanshiruyi.com>
2022-07-17 01:38:04 +0800

    111

---
+++ src/com/mystudy/Tree.java
@@ -0,0 +1,11 @@
+package com.mystudy;
+
+/**
+ * @author :dingyb
+ * @date : 2022/7/17 1:37
+ */
+public class Tree {
+    public static void main(String[] args) {
+        System.out.println("111");
+    }
+}
